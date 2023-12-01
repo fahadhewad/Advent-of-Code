c73e1467aeed47c4103f9ef50c2150cdf9eca2e1
fahadhewad <fahadhewad@gmail.com>
2023-12-01 17:08:56 +0000

    1 - 2

--- day1.py
+++ day1.py
@@ -1001,6 +1001,22 @@ eightbqfhnmvqsoneninezbrzcqkz4ftv
 arr = input.split('\n')
 sum = 0
 for line in arr:
+
+    stringNums = { #sol found online
+    "one": 'o1e',
+    "two": 't2o',
+    "three": 't3e',
+    "four": 'f4r',
+    "five": 'f5e',
+    "six": 's6x',
+    "seven": 's7n',
+    "eight": 'e8t',
+    "nine": 'n9e'
+    }
+
+    for key, value in stringNums.items():
+        line = line.replace(key, value)
+        
     number = []
     for char in line:
         if char.isdigit():
