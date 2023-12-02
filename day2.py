6459e81ba3590282d42368267706daeed42215ef
fahadhewad <fahadhewad@gmail.com>
2023-12-02 14:46:11 +0000

    2 - 2

--- day2.py
+++ day2.py
@@ -10,6 +10,11 @@ arr = input.split('\n')
 sum = 0
 
 for line in arr:
+    total = {
+        'red' : 0,
+        'green' : 0,
+        'blue' : 0,
+    }
     id, line = line.split(':')
     id = id.split()
     id = id[1]
@@ -22,8 +27,7 @@ for line in arr:
         for ball in sub.split(','):
             n, colour = ball.split()
             d[colour] = int(n)
-        if d["red"] > 12 or d["green"] > 13 or d["blue"] > 14:
-            break
-    else: # else can be used if for loop doesn't break
-        sum += int(id)
+        for k in total:
+            total[k] = max(total[k], d[k])
+    sum += total["red"] * total["blue"] * total["green"]
 print(sum)   
