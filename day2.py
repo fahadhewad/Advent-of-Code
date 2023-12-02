91fd98f2b38dcffba1307d35cdf26ee93c94e892
fahadhewad <fahadhewad@gmail.com>
2023-12-02 14:25:27 +0000

    2 - 1

--- day2.py
+++ day2.py
@@ -7,4 +7,23 @@ with open(file_path, "r") as file:
 input = big_string
 
 arr = input.split('\n')
-print(arr)
+sum = 0
+
+for line in arr:
+    id, line = line.split(':')
+    id = id.split()
+    id = id[1]
+    for sub in line.split(';'):
+        d = {
+        'red' : 0,
+        'green' : 0,
+        'blue' : 0,
+        }
+        for ball in sub.split(','):
+            n, colour = ball.split()
+            d[colour] = int(n)
+        if d["red"] > 12 or d["green"] > 13 or d["blue"] > 14:
+            break
+    else: # else can be used if for loop doesn't break
+        sum += int(id)
+print(sum)   
